a119f71e318a386d0562a95d5c93fec3349b90ea
david1-lab <skahn7@naver.com>
2025-03-22 13:14:23 +0900

    Fix: build working locally

--- src/components/main/LanguageIcons.jsx
+++ src/components/main/LanguageIcons.jsx
@@ -1,5 +1,39 @@
+import { CircleFlag } from "react-circle-flags";
+import { useNavigate } from "react-router-dom";
+import styles from "./languageIcons.module.css";
+
 function LanguageIcons() {
-  return <div>LanguageIcons</div>;
-  }
-  
-  export default LanguageIcons; 
+  const navigate = useNavigate();
+
+  const onButtonClick = (lang) => {
+    navigate(`/?lang=${lang}`);
+  };
+
+  return (
+    <div>
+      {/* -> mbti.com/?lang=Kor */}
+      <CircleFlag
+        className={styles.flagIcon}
+        onClick={() => onButtonClick("Kor")}
+        countryCode="kr"
+        width={48}
+      />
+      {/* -> mbti.com/?lang=Eng */}
+      <CircleFlag
+        className={styles.flagIcon}
+        onClick={() => onButtonClick("Eng")}
+        countryCode="us"
+        width={48}
+      />
+      {/* -> mbti.com/?lang=JP */}
+      <CircleFlag
+        className={styles.flagIcon}
+        onClick={() => onButtonClick("JP")}
+        countryCode="jp"
+        width={48}
+      />
+    </div>
+  );
+}
+
+export default LanguageIcons;
