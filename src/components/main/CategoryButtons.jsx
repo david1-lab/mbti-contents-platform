f7628c55c4441342e9326f10fa9b656802f46ba9
david1-lab <skahn7@naver.com>
2025-03-24 08:32:15 +0900

    Temporarily rename CategoryButtons.module.css to force case change

--- src/components/main/CategoryButtons.jsx
+++ src/components/main/CategoryButtons.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
-import styles from "./CategoryButtons.module.css";
+import styles from "./CategoryBtns.module.css";
 
 function CategoryButtons() {
   const [searchParams] = useSearchParams();
